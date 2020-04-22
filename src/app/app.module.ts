b94ecfbb573f1fba706f204df6d54fc615601e63
Jose Luis Rodriguez <jrodriguezri@dxc.com>
2020-04-21 22:27:41 -0500

    clase 57 creacion de servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { HeroesService } from './services/heroes.service';
+
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-
-import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,9 @@ import { APP_ROUTING } from './app.routes';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[]
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroes = this.heroesService.getHeroes();
+
+    console.log(this.heroes);
   }
 
 }
